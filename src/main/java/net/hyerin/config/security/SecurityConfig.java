0110a17e6cd3367601ad96ee0db1ec845f5edf4f
hyerin6 <hyerinn6@gmail.com>
2020-04-24 00:40:29 +0900

    [#9] Add RedisProperties & Dockerfile

--- src/main/java/net/hyerin/config/security/SecurityConfig.java
+++ src/main/java/net/hyerin/config/security/SecurityConfig.java
@@ -1,20 +1,14 @@
 package net.hyerin.config.security;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import net.hyerin.user.security.MyAuthenticationFailureHandler;
 import net.hyerin.user.security.MyAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
