cb1f36d899f7a4da6337fa6fc272c3581795f5c2
hyerin6 <hyerinn6@gmail.com>
2020-04-24 23:51:07 +0900

    [#9] Redis Password 설정

--- src/main/java/net/hyerin/config/redis/RedisProperties.java
+++ src/main/java/net/hyerin/config/redis/RedisProperties.java
@@ -10,12 +10,15 @@ public class RedisProperties {
 
     private int redisPort;
     private String redisHost;
+    private String redisPassword;
 
     public RedisProperties(
             @Value("${spring.redis.port}") int redisPort,
-            @Value("${spring.redis.host}") String redisHost) {
+            @Value("${spring.redis.host}") String redisHost,
+            @Value("${spring.redis.password}") String redisPassword) {
         this.redisPort = redisPort;
         this.redisHost = redisHost;
+        this.redisPassword = redisPassword;
     }
 
 }
