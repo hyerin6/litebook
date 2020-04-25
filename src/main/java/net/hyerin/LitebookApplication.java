c52e798aa08e7d4110c664ac16a0f058d5a7a5df
hyerin6 <hyerinn6@gmail.com>
2020-04-25 19:12:43 +0900

    [#9] view 경로 설정 & Add view file

--- src/main/java/net/hyerin/LitebookApplication.java
+++ src/main/java/net/hyerin/LitebookApplication.java
@@ -3,6 +3,8 @@ package net.hyerin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,7 +19,6 @@ public class LitebookApplication {
 	@Bean
 	public InternalResourceViewResolver setupViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
