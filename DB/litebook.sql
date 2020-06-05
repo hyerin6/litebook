e580bf381a44ad0d69a432ef0202e0de3539ff36
hyerin6 <hyerinn6@gmail.com>
2020-06-05 22:56:47 +0900

    Update litebook.sql

--- DB/litebook.sql
+++ DB/litebook.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `litebook` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `litebook`;
 -- MySQL dump 10.13  Distrib 8.0.15, for macos10.14 (x86_64)
 --
 -- Host: localhost    Database: litebook
@@ -54,9 +52,10 @@ DROP TABLE IF EXISTS `Images`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `Images` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fileName` varchar(255) DEFAULT NULL,
   `filePath` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +64,6 @@ CREATE TABLE `Images` (
 
 LOCK TABLES `Images` WRITE;
 /*!40000 ALTER TABLE `Images` DISABLE KEYS */;
-INSERT INTO `Images` VALUES (1,'https://s3.ap-northeast-2.amazonaws.com/litebook-images/profile/'),(2,'https://s3.ap-northeast-2.amazonaws.com/litebook-images/profile/profile.jpeg');
 /*!40000 ALTER TABLE `Images` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,13 +76,13 @@ DROP TABLE IF EXISTS `Post`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `Post` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `mainText` varchar(255) DEFAULT NULL,
+  `mainText` varchar(3000) DEFAULT NULL,
   `started_date` datetime DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKg7x75nqlmhufeeoxcuh22awxx` (`user_id`),
   CONSTRAINT `FKg7x75nqlmhufeeoxcuh22awxx` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +91,6 @@ CREATE TABLE `Post` (
 
 LOCK TABLES `Post` WRITE;
 /*!40000 ALTER TABLE `Post` DISABLE KEYS */;
-INSERT INTO `Post` VALUES (1,'안녕하세요','2020-05-08 00:42:12',1),(2,'안녕안녕','2020-05-08 00:59:31',2),(3,'테스트 포스팅 입니다~\r\n','2020-05-08 00:59:43',2);
 /*!40000 ALTER TABLE `Post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +114,7 @@ CREATE TABLE `User` (
   UNIQUE KEY `UK_e6gkqunxajvyxl5uctpl2vl2p` (`email`),
   KEY `FKcspeywrrnp826opnmixxxc8lp` (`profile_id`),
   CONSTRAINT `FKcspeywrrnp826opnmixxxc8lp` FOREIGN KEY (`profile_id`) REFERENCES `images` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -126,7 +123,6 @@ CREATE TABLE `User` (
 
 LOCK TABLES `User` WRITE;
 /*!40000 ALTER TABLE `User` DISABLE KEYS */;
-INSERT INTO `User` VALUES (1,'hyerinn6@gmail.com',_binary '','hyerin','ecd71870d1963316a97e3ac3408c9835ad8cf0f3c1bc703527c3265534f75ae','','IS_AUTHENTICATED_FULLY',1),(2,'0419hyerin@gmail.com',_binary '','혜린','ecd71870d1963316a97e3ac3408c9835ad8cf0f3c1bc703527c3265534f75ae','','IS_AUTHENTICATED_FULLY',2);
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -139,4 +135,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-08  1:30:30
+-- Dump completed on 2020-06-05 22:55:42
